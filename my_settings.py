be2b936acb7063cbc03e37f2e4d35c7fd4257400
김민지 <minji@gimminjiui-MacBookAir.local>
2022-07-02 16:47:50 +0900

    Add :q :q
    :q
    
    : Django Project

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_test',
+        'USER': 'root',
+        'PASSWORD': 'Alswl9811!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-*7)059zr79$(+#qbwe%l*+&9p55o04vfg8=d389gvf=j0-el&*'
